bbcd545b1cb2ed29362a0ba3663201fc1be8594b
k.l.lambda <k.l.lambda@gmail.com>
2020-09-29 16:52:06 +0800

    lilypond.cmake: added static library of lilypond.

---
+++ cmake/lilypond.cmake
@@ -0,0 +1,25 @@
+
+file(GLOB SOURCE_FILES ${LILYPOND_DIR}/lily/*.cc)
+
+add_library(
+	lilypond.a
+
+	STATIC
+
+	${SOURCE_FILES}
+)
+
+
+target_include_directories(
+	lilypond.a
+
+	PRIVATE ${LILYPOND_DIR}/build
+	PRIVATE ${LILYPOND_DIR}/build/lily/out
+	PRIVATE ${LILYPOND_DIR}/lily/include
+	PRIVATE ${LILYPOND_DIR}/flower/include
+	PRIVATE /usr/include/guile/2.2
+	PRIVATE /usr/include/freetype2
+	PRIVATE /usr/include/pango-1.0
+	PRIVATE /usr/include/glib-2.0
+	PRIVATE /usr/lib/x86_64-linux-gnu/glib-2.0/include
+)
