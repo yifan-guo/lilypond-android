24cc993b17e9bc429d9651aa1bc251b13b3cb304
k.l.lambda <k.l.lambda@gmail.com>
2020-10-04 15:28:39 +0800

    simple.js: write result to disk.

--- node-addon/tests/simple.js
+++ node-addon/tests/simple.js
@@ -1,10 +1,15 @@
 
+const fs = require("fs")
 const lilypond = require("../output/lilypond");
+
+
 
 const main = async () => {
 	const error = await lilypond.engrave("\\relative {c'1}", {
 		onSVG (filename, content) {
-			console.log("svg file:", filename, content);
+			console.log("svg file received:", filename);
+			fs.promises.writeFile("./simple.svg", content)
+				.then(() => console.log("simple.svg wrote."));
 		},
 	});
 
