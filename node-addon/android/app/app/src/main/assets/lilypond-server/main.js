a2b826425070300287c2298da2daa310c3fad473
k.l.lambda <k.l.lambda@gmail.com>
2020-10-12 17:26:36 +0800

    node-addon/android: added home page for test server.

--- node-addon/android/app/app/src/main/assets/lilypond-server/main.js
+++ node-addon/android/app/app/src/main/assets/lilypond-server/main.js
@@ -1,12 +1,26 @@
 
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const lilypond = require("./lilypond");
 
 
 
-const http_server = http.createServer( async (request, response) => {
-	const ly_code = "\\relative c' {c1}";
-	// TODO: extract ly code from request
+let PWD = process.cwd();
+
+
+const handleHome = async (req, res) => {
+	res.writeHead(200, {
+		"Content-Type": "text/html",
+	});
+
+	const html = await fs.promises.readFile(path.resolve(PWD, "./home.html"));
+	res.end(html);
+};
+
+
+const handleEngrave = async (req, res) => {
+	const ly_code = await new Promise(resolve => req.on("data", chunk => resolve(chunk.toString())));
 
 	let svg = null;
 	let log = null;
@@ -23,21 +37,42 @@ const http_server = http.createServer( async (request, response) => {
 	console.log("Engrave result:", error);
 
 	if (!error) {
-		response.writeHead(200, {
+		res.writeHead(200, {
 			"Content-Type": "image/svg+xml",
 		});
 
-		response.end(svg);
+		res.end(svg);
 	}
 	else {
-		response.writeHead(401, {
+		res.writeHead(401, {
 			"Content-Type": "text/plain",
 		});
 
-		response.end(log);
+		res.end(log);
+	}
+};
+
+
+const http_server = http.createServer( async (request, response) => {
+	switch (request.method) {
+	case "GET":
+		handleHome (request, response);
+
+		break;
+	case "POST":
+		handleEngrave (request, response);
+
+		break;
 	}
 });
 
 http_server.listen(3000, "0.0.0.0", () => {
 	console.log("Lilypond server is online: http://localhost:3000");
+	/*for (const key in process.env) {
+		console.log("env:", key, process.env[key]);
+	}
+	console.log("argv:", process.argv);*/
+
+	PWD = path.resolve(process.argv[1], "..");
+	console.log("PWD:", PWD);
 });
