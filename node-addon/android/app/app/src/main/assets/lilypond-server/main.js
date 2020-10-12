ba0f9af80201c71415ebb1fee73f8c5ab40b0087
k.l.lambda <k.l.lambda@gmail.com>
2020-10-12 14:54:09 +0800

    node-addon/android/app: module loading of lilypond.node added.

--- node-addon/android/app/app/src/main/assets/lilypond-server/main.js
+++ node-addon/android/app/app/src/main/assets/lilypond-server/main.js
@@ -1,11 +1,41 @@
 
 const http = require("http");
-//var fc = require('./function-call');
+const lilypond = require("./lilypond");
 
 
 
 const http_server = http.createServer( async (request, response) => {
-	response.end("hello.");
+	const ly_code = "\\relative c' {c1}";
+	// TODO: extract ly code from request
+
+	let svg = null;
+	let log = null;
+
+	const error = await lilypond.engrave(ly_code, {
+		log (message) {
+			log = message;
+			console.log("lilypond log:", message);
+		},
+		onSVG (filename, content) {
+			svg = content;
+		},
+	});
+	console.log("Engrave result:", error);
+
+	if (!error) {
+		response.writeHead(200, {
+			"Content-Type": "image/svg+xml",
+		});
+
+		response.end(svg);
+	}
+	else {
+		response.writeHead(401, {
+			"Content-Type": "text/plain",
+		});
+
+		response.end(log);
+	}
 });
 
 http_server.listen(3000, "0.0.0.0", () => {
