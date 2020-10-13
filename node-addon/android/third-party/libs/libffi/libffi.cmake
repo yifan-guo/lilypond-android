bd0c3aa3c78b0cdf82896aedf00ae592aeb18a0c
k.l.lambda <k.l.lambda@gmail.com>
2020-10-13 19:17:47 +0800

    third-party: added library of libffi with making.

---
+++ node-addon/android/third-party/libs/libffi/libffi.cmake
@@ -0,0 +1,29 @@
+
+file(GLOB_RECURSE LIBFFI_SOURCE_FILES ${LIBS_DIR}/libffi/src/*.c)
+#message(WARNING "LIBFFI_SOURCE_FILES: ${LIBFFI_SOURCE_FILES}")
+
+
+add_library(
+	ffi
+
+	STATIC
+
+	${LIBFFI_SOURCE_FILES}
+)
+
+
+target_include_directories(
+	ffi
+
+	PRIVATE ${LIBS_DIR}/libffi/include
+	PUBLIC ${LIBS_DIR}/libffi/${ANDROID_ABI}
+	PUBLIC ${LIBS_DIR}/libffi/${ANDROID_ABI}/include
+)
+
+
+set_target_properties(
+	ffi
+	PROPERTIES
+
+	LINKER_LANGUAGE C
+)
