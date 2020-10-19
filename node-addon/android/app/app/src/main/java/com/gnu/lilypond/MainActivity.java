51fce838a7a60cc6f33021c62d51c003c6338fcc
k.l.lambda <k.l.lambda@gmail.com>
2020-10-19 17:11:27 +0800

    android/app: added test button.

--- node-addon/android/app/app/src/main/java/com/gnu/lilypond/MainActivity.java
+++ node-addon/android/app/app/src/main/java/com/gnu/lilypond/MainActivity.java
@@ -16,8 +16,7 @@ import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
-	// We just want one instance of node running in the background.
-	//public static boolean _startedNodeAlready = false;
+	static String lyCode = "\\version \"2.20.0\"\n\\relative c' {c1}";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +26,51 @@ public class MainActivity extends AppCompatActivity {
 		Intent intent = new Intent(this, NodeService.class);
 		startService(intent);
 
-		final Button buttonOpen = (Button) findViewById(R.id.btRequest);
+		final Button buttonRequest = (Button) findViewById(R.id.btRequest);
+		final Button buttonOpen = (Button) findViewById(R.id.btOpen);
 		final TextView textViewStatus = (TextView) findViewById(R.id.tvRequest);
 
-		buttonOpen.setOnClickListener(new View.OnClickListener() {
+		buttonRequest.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				/*// Network operations should be done in the background.
+				// Network operations should be done in the background.
 				new AsyncTask<Void,Void,String>() {
 					@Override
 					protected String doInBackground(Void... params) {
-						String nodeResponse="";
+						String nodeResponse = "";
 						try {
 							URL localNodeServer = new URL("http://localhost:3000/");
-							BufferedReader in = new BufferedReader(
+							/*BufferedReader in = new BufferedReader(
 									new InputStreamReader(localNodeServer.openStream()));
+
 							String inputLine;
 							while ((inputLine = in.readLine()) != null)
-								nodeResponse=nodeResponse+inputLine;
-							in.close();
+								nodeResponse = nodeResponse + inputLine;
+
+							in.close();*/
+							HttpURLConnection conn = (HttpURLConnection)localNodeServer.openConnection();
+							conn.setRequestMethod("POST");
+							conn.setDoInput(true);
+							conn.setDoOutput(true);
+
+							OutputStream os = conn.getOutputStream();
+							BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+							writer.write(lyCode);
+							writer.flush();
+							writer.close();
+							os.close();
+
+							int responseCode = conn.getResponseCode();
+							if (responseCode == HttpURLConnection.HTTP_OK)
+								nodeResponse = "It worked: ";
+							else
+								nodeResponse = "It failed: ";
+
+							String inputLine;
+							BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+							while ((inputLine = br.readLine()) != null)
+								nodeResponse = nodeResponse + inputLine;
 						} catch (Exception ex) {
-							nodeResponse=ex.toString();
+							nodeResponse = ex.toString();
 						}
 						return nodeResponse;
 					}
@@ -55,7 +79,12 @@ public class MainActivity extends AppCompatActivity {
 					protected void onPostExecute(String result) {
 						textViewStatus.setText(result);
 					}
-				}.execute();*/
+				}.execute();
+			}
+		});
+
+		buttonOpen.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
 				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://localhost:3000/"));
 				startActivity(browserIntent);
 			}
