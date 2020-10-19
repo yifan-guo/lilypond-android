cfe36acda4a95881f34a78be8c97a3e32fc4756c
k.l.lambda <k.l.lambda@gmail.com>
2020-10-19 16:04:54 +0800

    android/app: added button for open browser.

--- node-addon/android/app/app/src/main/java/com/gnu/lilypond/MainActivity.java
+++ node-addon/android/app/app/src/main/java/com/gnu/lilypond/MainActivity.java
@@ -2,16 +2,12 @@ package com.gnu.lilypond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-//import android.content.Context;
-//import android.content.pm.PackageInfo;
-//import android.content.pm.PackageManager;
-//import android.content.SharedPreferences;
-//import android.content.res.AssetManager;
 import android.content.Intent;
 import java.net.*;
 import java.io.*;
@@ -28,39 +24,15 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		/*if( !_startedNodeAlready ) {
-			_startedNodeAlready = true;
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					// The path where we expect the node project to be at runtime.
-					String nodeDir = getApplicationContext().getFilesDir().getAbsolutePath() + "/lilypond-server";
-					if (wasAPKUpdated()) {
-						// Recursively delete any existing lilypond-server.
-						File nodeDirReference = new File(nodeDir);
-						if (nodeDirReference.exists()) {
-							deleteFolderRecursively(new File(nodeDir));
-						}
-						// Copy the node project from assets into the application's data path.
-						copyAssetFolder(getApplicationContext().getAssets(), "lilypond-server", nodeDir);
-
-						saveLastUpdateTime();
-					}
-					startNodeWithArguments(new String[] {"node", nodeDir + "/main.js"});
-				}
-			}).start();
-		}*/
 		Intent intent = new Intent(this, NodeService.class);
 		startService(intent);
 
+		final Button buttonOpen = (Button) findViewById(R.id.btRequest);
+		final TextView textViewStatus = (TextView) findViewById(R.id.tvRequest);
 
-		final Button buttonVersions = (Button) findViewById(R.id.btRequest);
-		final TextView textViewVersions = (TextView) findViewById(R.id.tvRequest);
-
-		buttonVersions.setOnClickListener(new View.OnClickListener() {
+		buttonOpen.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-
-				// Network operations should be done in the background.
+				/*// Network operations should be done in the background.
 				new AsyncTask<Void,Void,String>() {
 					@Override
 					protected String doInBackground(Void... params) {
@@ -78,123 +50,18 @@ public class MainActivity extends AppCompatActivity {
 						}
 						return nodeResponse;
 					}
+
 					@Override
 					protected void onPostExecute(String result) {
-						textViewVersions.setText(result);
+						textViewStatus.setText(result);
 					}
-				}.execute();
+				}.execute();*/
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://localhost:3000/"));
+				startActivity(browserIntent);
 			}
 		});
 
+		textViewStatus.setText("Lilypond server started.");
 	}
-
-
-	/**
-	 * A native method that is implemented by the 'native-lib' native library,
-	 * which is packaged with this application.
-	 */
-	//public native Integer startNodeWithArguments(String[] arguments);
-
-	/*private boolean wasAPKUpdated() {
-		SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
-		long previousLastUpdateTime = prefs.getLong("NODEJS_MOBILE_APK_LastUpdateTime", 0);
-		long lastUpdateTime = 1;
-		try {
-			PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
-			lastUpdateTime = packageInfo.lastUpdateTime;
-		} catch (PackageManager.NameNotFoundException e) {
-			e.printStackTrace();
-		}
-		return (lastUpdateTime != previousLastUpdateTime);
-	}
-
-
-	private void saveLastUpdateTime() {
-		long lastUpdateTime = 1;
-		try {
-			PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
-			lastUpdateTime = packageInfo.lastUpdateTime;
-		} catch (PackageManager.NameNotFoundException e) {
-			e.printStackTrace();
-		}
-		SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putLong("NODEJS_MOBILE_APK_LastUpdateTime", lastUpdateTime);
-		editor.commit();
-	}
-
-
-	private static boolean deleteFolderRecursively(File file) {
-		try {
-			boolean res=true;
-			for (File childFile : file.listFiles()) {
-				if (childFile.isDirectory()) {
-					res &= deleteFolderRecursively(childFile);
-				} else {
-					res &= childFile.delete();
-				}
-			}
-			res &= file.delete();
-			return res;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-
-	private static boolean copyAssetFolder(AssetManager assetManager, String fromAssetPath, String toPath) {
-		try {
-			String[] files = assetManager.list(fromAssetPath);
-			boolean res = true;
-
-			if (files.length==0) {
-				// If it's a file, it won't have any assets "inside" it.
-				res &= copyAsset(assetManager,
-						fromAssetPath,
-						toPath);
-			} else {
-				new File(toPath).mkdirs();
-				for (String file : files)
-				res &= copyAssetFolder(assetManager,
-						fromAssetPath + "/" + file,
-						toPath + "/" + file);
-			}
-			return res;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-
-	private static boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
-		InputStream in = null;
-		OutputStream out = null;
-		try {
-			in = assetManager.open(fromAssetPath);
-			new File(toPath).createNewFile();
-			out = new FileOutputStream(toPath);
-			copyFile(in, out);
-			in.close();
-			in = null;
-			out.flush();
-			out.close();
-			out = null;
-			return true;
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-
-	private static void copyFile(InputStream in, OutputStream out) throws IOException {
-		byte[] buffer = new byte[1024];
-		int read;
-		while ((read = in.read(buffer)) != -1) {
-			out.write(buffer, 0, read);
-		}
-	}*/
 
 }
