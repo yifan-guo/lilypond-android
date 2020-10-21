76b92f01c0b7dbf776b210623e4880db99d22194
k.l.lambda <k.l.lambda@gmail.com>
2020-10-21 10:49:26 +0800

    node-addon/android/makeSDK.sh created.

---
+++ node-addon/android/makeSDK.sh
@@ -0,0 +1,15 @@
+mkdir -p ./app/lilypond/build/sdk
+rm -rf ./app/lilypond/build/sdk/*
+
+mkdir ./app/lilypond/build/sdk/lilypond
+cp -r ./app/lilypond/build/intermediates/cmake/release/obj/* ./app/lilypond/build/sdk/lilypond
+
+abi_dirs=$(find ./app/lilypond/build/sdk/lilypond -mindepth 1 -maxdepth 1 -type d)
+for dir in $abi_dirs
+do
+	rm $dir/*.a
+	mv $dir/liblilypond-node-addon-android.so $dir/lilypond.node
+done
+
+cd ./app/lilypond/build/sdk
+tar -czvf ./lilypond-android-binaries-prebuilt.tar.gz lilypond
